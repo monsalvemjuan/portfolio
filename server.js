f587f071ee3dc3ccbeacb442c900204cbdf73158
monsalvemjuan <77866146+monsalvemjuan@users.noreply.github.com>
2022-04-13 17:27:20 -0500

    Second update for Package

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
+
+app.get('*',git a (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
+
 app.listen(port, () => {
     console.log('Server is up!')
 });
+
+
